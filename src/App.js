e0be287711808130c0a1063d0836a755d7f5cffb
leseraphina <leseraphina@gmail.com>
2023-07-14 17:19:57 +0900

    react01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// 
+const people ={
+  no:1,
+  level:'1단계',
+  room : 'A강의실'
+}
+function Header(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      {/* <h1>react welcome {1 + 3}</h1> */}
+      <h1>{`react welcome ${1 + 3}`} </h1>
+    </header>
+  );
+}
+function Content(){
+  return (
+    <>
+    <ul className="main">
+      <li>no: {people.no}</li>
+      <li>level : {people.level}</li>
+      <li>강의실: {people.room}</li>
+    </ul>
+    <p>
+      <span>컴포넌트 이름</span>
+      <lable htmlFor="name">Name</lable>
+      <input type="text" id="name" />
+    </p>
+    </>
+  );
+}
+function Footer(){
+  return (
+    <footer>
+      <small>저작권정보</small>
+    </footer>
   );
 }
 
-export default App;
+export default function App(){
+  return (
+    <>
+    <Header />
+    <Content />
+    <Footer />
+    </>
+  )
+}
---
+++ src/App_01.js
@@ -0,0 +1,14 @@
+// export default function App(){}
+// function Content(){}
+// function Footer(){}
+
+function App(){
+  return (
+    <>
+    <h1>welcome React</h1>
+    <h2>welcome React</h2>
+    </>
+  )
+}
+
+export default App;
